1ce78ef39ac1acc2e873c57d0574f280e32f6b05
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-06-25 17:19:42 -0300

    Sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,48 @@
+import { Button, Flex, FormLabel, Input, Stack } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <h1>dashgo</h1>
-  )
+    <Flex justify="center" align="center" w="100vw" h="100vh">
+      <Flex
+        bg="gray.800"
+        padding="6"
+        borderRadius="10"
+        p="8"
+        maxWidth="360"
+        w="100%"
+        as="form"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormLabel htmlFor="email">E-mail</FormLabel>
+          <Input
+            name="email"
+            type="email"
+            variant="filled"
+            bgColor="gray.900"
+            focusBorderColor="pink.500"
+            _hover={{
+              bg: "gray.900",
+            }}
+            size="lg"
+          />
+          <FormLabel htmlFor="password">Senha</FormLabel>
+          <Input
+            name="password"
+            type="password"
+            variant="filled"
+            bg="gray.900"
+            focusBorderColor="pink.500"
+            _hover={{
+              bg: "gray.900",
+            }}
+            size="lg"
+          />
+        </Stack>
+        <Button colorScheme="pink" type="submit" mt="6" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
