e277dc6205acdffec316cddb9b1f5b55c9ab605a
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-06-27 22:41:07 -0300

    dashboard component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,65 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 import Sidebar from "src/components/Form/Sidebar";
 import Header from "src/components/Header";
+import dynamic from "next/dynamic";
+import { theme } from "src/styles/theme";
+import { ApexOptions } from "apexcharts";
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false
+})
+
+const options: ApexOptions = {
+    chart: {
+        toolbar: {
+            show: false
+        },
+        zoom: {
+            enabled: false
+        },
+        foreColor: theme.colors.gray[500]
+    },
+    grid: {
+        show: false
+    },
+    dataLabels: {
+        enabled: false
+    },
+    tooltip: {
+        enabled: false
+    },
+    xaxis: {
+        type: 'datetime',
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            '2021-03-18T00:00:00.000Z',
+            '2021-03-19T00:00:00.000Z',
+            '2021-03-20T00:00:00.000Z',
+            '2021-03-21T00:00:00.000Z',
+            '2021-03-22T00:00:00.000Z',
+            '2021-03-23T00:00:00.000Z',
+            '2021-03-24T00:00:00.000Z'
+        ]
+    },
+    fill: {
+        opacity: 0.3,
+        type: 'gradient',
+        gradient: {
+            shade: 'dark',
+            opacityFrom: 0.7,
+            opacityTo: 0.3
+        }
+    }
+}
+
+const series = [
+    {name: 'series1', data: [31, 120, 10, 28, 61, 18, 109]}
+]
 
 export default function Dashboard(){
     return(
@@ -8,6 +67,15 @@ export default function Dashboard(){
             <Header/>
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar/>
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+                    <Box p="8" bg="gray.800" borderRadius={8}>
+                        <Chart options={options} series={series} type="area" height={160}/>
+                    </Box>
+                    <Box p="8" bg="gray.800" borderRadius={8}>
+                        <Chart options={options} series={series} type="area" height={160}/>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     )
