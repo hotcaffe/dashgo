30e6f5aff1dca8c6ab36d39d1ef2c8a6f875626c
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-06-28 22:13:45 -0300

    Pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,22 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Sidebar from "src/components/Form/Sidebar";
 import Header from "src/components/Header";
+import { Pagination } from "src/components/Pagination";
 
 export default function UserList() {
   return (
@@ -14,45 +29,56 @@ export default function UserList() {
             <Heading size="lg" fontWeight="normal">
               Usuários
             </Heading>
-            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>}>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
               Criar novo
             </Button>
           </Flex>
           <Table colorScheme="whiteAlpha">
             <Thead>
-                <Tr>
-                    <Th px="6" color="gray.300" width="8">
-                        <Checkbox colorScheme="pink"/>
-                    </Th>
-                    <Th>
-                        Usuários
-                    </Th>
-                    <Th>
-                        Data de cadastro
-                    </Th>
-                    <Th width="8"/>
-                </Tr>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuários</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8" />
+              </Tr>
             </Thead>
             <Tbody>
-                <Tr>
-                    <Td px="6">
-                        <Checkbox colorScheme="pink"/>
-                    </Td>
-                    <Td>
-                        <Box>
-                            <Text fontWeight="bold">Raphael Fusco</Text>
-                            <Text fontSize="sm" color="gray.300">raphaelfusco.rar@gmail.com</Text>
-                        </Box>
-                    </Td>
-                    <Td>28 de Junho, 2022</Td>
-                    <Td>
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>
-                            Editar
-                        </Button>
-                    </Td>
-                </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Raphael Fusco</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      raphaelfusco.rar@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>28 de Junho, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
+          <Pagination/>
         </Box>
       </Flex>
     </Box>
